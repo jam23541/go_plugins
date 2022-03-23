78d6912c2c804cfb763dc5ce37bfd021a03edd30
jam23541 <cyn0407@live.com>
2022-03-24 00:39:45 +0800

    Update uniqueIdProducer.go
    added GetMqtttTopic from natsTopic and vice versa

--- uniqueIdProducer.go
+++ uniqueIdProducer.go
@@ -1,7 +1,18 @@
 package go_plugins
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 func UniqueId(puber string) string {
 	return puber + time.Now().Format(time.StampMilli)
+}
+
+func GetMqttTopic(natsTopic string) string {
+	return strings.ReplaceAll(natsTopic, ".", "/")
+}
+
+func GetNatsTopic(mqttTopic string) string {
+	return strings.ReplaceAll(mqttTopic, "/", ".")
 }
