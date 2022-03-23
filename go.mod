4e1211618fb255e6949f2f7817fb8f7b31a08517
jam23541 <cyn0407@live.com>
2022-03-24 00:09:09 +0800

    updated gitignore and function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jam23541/go_plugins
+
+go 1.17
---
+++ uniqueIdProducer.go
@@ -0,0 +1,7 @@
+package go_plugins
+
+import "time"
+
+func UniqueId(puber string) string {
+	return puber + time.Now().Format(time.StampMilli)
+}
